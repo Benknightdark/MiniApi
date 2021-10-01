344fe2319bb3f22717e43b741060999e37f789e7
ben <moon791017@gmail.com>
2021-10-01 17:17:10 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<HelloService>(new HelloService());
+
+var app = builder.Build();
+
+app.MapGet("/hello", (HttpContext context, HelloService helloService) =>
+helloService.SayHello(context.Request.Query["name"].ToString()));
+
+app.Run();
